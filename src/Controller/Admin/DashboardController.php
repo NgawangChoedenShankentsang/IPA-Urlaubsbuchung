6bc8a7fc08c3cb6d0bf7e477dd1731879d4e4f07
Ngawang <=>
2024-11-28 10:31:42 +0100

    add register form

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -55,10 +55,18 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa-solid fa-desktop');
-        yield MenuItem::linkToCrud('Users', 'fas fa-user', Employees::class)->setAction(Crud::PAGE_INDEX);
-        yield MenuItem::linkToCrud('Types', 'fa-solid fa-layer-group', HolidayTypes::class)->setAction(Crud::PAGE_INDEX);
-        yield MenuItem::linkToCrud('Status', 'fa-solid fa-tag', HolidayStatus::class)->setAction(Crud::PAGE_INDEX);
-        yield MenuItem::linkToCrud('Request', 'fa-solid fa-bell', Holiday::class)->setAction(Crud::PAGE_INDEX);
+        yield MenuItem::linkToCrud('Users', 'fas fa-user', Employees::class)
+            ->setAction(Crud::PAGE_INDEX)
+            ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Types', 'fa-solid fa-layer-group', HolidayTypes::class)
+            ->setAction(Crud::PAGE_INDEX)
+            ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Status', 'fa-solid fa-tag', HolidayStatus::class)
+            ->setAction(Crud::PAGE_INDEX)
+            ->setPermission('ROLE_ADMIN');
+        yield MenuItem::linkToCrud('Request', 'fa-solid fa-bell', Holiday::class)
+            ->setAction(Crud::PAGE_INDEX)
+            ->setPermission('ROLE_ADMIN');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
